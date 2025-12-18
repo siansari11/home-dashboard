11cdb462bcb019bb91f5a4f6609229317df7e87e
siansari11 <124088563+siansari11@users.noreply.github.com>
2025-12-18 16:28:27 +0100

    Create feeds.js

---
+++ src/config/feeds.js
@@ -0,0 +1,22 @@
+// src/config/feeds.js
+// Add/edit feed URLs only here.
+
+export const FEED_GROUPS = [
+  {
+    key: "recipes",
+    title: "Healthy High-Protein Recipes",
+    urls: [
+      // Add more later if you want
+      "https://www.eatingwell.com/rss",
+      "https://www.skinnytaste.com/feed/",
+    ],
+  },
+  {
+    key: "apartment",
+    title: "Small Apartment & Decluttering",
+    urls: [
+      "https://www.thespruce.com/rss",
+      "https://www.apartmenttherapy.com/main.rss",
+    ],
+  },
+];
