ee646939635b30c349ad9bb80b72048519f8527b
siansari11 <124088563+siansari11@users.noreply.github.com>
2026-05-05 17:40:20 +0200

    Update todoist.js

--- src/lib/todoist.js
+++ src/lib/todoist.js
@@ -1,6 +1,6 @@
 import { CONFIG } from "../config.js";
 
-const API = "https://corsproxy.io/?https://api.todoist.com/api/v1/sync";
+const API = "https://corsproxy.io/?https://api.todoist.com/api/v1/tasks";
 
 function headers(){
   return {
