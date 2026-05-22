bee2f2bed9d32a77c7ef9b21628e8ae558adfc09
siansari11 <124088563+siansari11@users.noreply.github.com>
2026-05-22 08:09:36 +0200

    Revert "Revert "adding the todoist tasks""

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
 export const CONFIG = {
-  todoist: {
-    apiToken: "eb253fa879d7b7bc4e3fa07b85f34f526f6b153b"
-  },
-  location: { name: "Seddiner See", lat: 52.2891284, lon: 12.9924364  },
-
-  // Pilot mode: try multiple public CORS proxies (they can be flaky)
-  corsProxies: [
-    (url) => "https://corsproxy.io/?"+encodeURIComponent(url),                  // common free proxy pattern 3
-  ],
-  corsProxy: (url) => "https://corsproxy.io/?"+encodeURIComponent(url),                  // common free proxy pattern 3
-};
+  location: {
+    name: 'Seddiner See',
+    lat: 52.2891284,
+    lon: 12.9924364
+  }
+}
